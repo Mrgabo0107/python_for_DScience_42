f001370f3abbe04f454ffaa8a1966f424b365346
Gabriel Moreno <gdmorenor@unal.edu.co>
2024-06-02 22:37:51 +0200

    05 ready

--- ex04/whatis.py
+++ ex04/whatis.py
@@ -1,23 +1,26 @@
 import sys
 
-try:
-    if len(sys.argv) < 3:
-        if len(sys.argv) == 2:
-            try:
-                value = int(sys.argv[1])
-                if value % 2 == 0:
-                    print("I'm Even")
-                    exit()
-                else:
-                    print ("I''m Odd")
-                    exit()
-            except ValueError:
-                raise AssertionError("argument is not an integer")
+def main():
+    try:
+        if len(sys.argv) < 3:
+            if len(sys.argv) == 2:
+                try:
+                    value = int(sys.argv[1])
+                    if value % 2 == 0:
+                        print("I'm Even")
+                        return 0
+                    else:
+                        print ("I'm Odd")
+                        return 0
+                except ValueError:
+                    raise AssertionError("argument is not an integer")
+            else:
+                return 0
         else:
-            exit()
-    else:
-        raise AssertionError("more than one argument is provided")
-except AssertionError as Err:
-    print(f"AssertionError: {Err}")
+            raise AssertionError("more than one argument is provided")
+    except AssertionError as Err:
+        print(f"AssertionError: {Err}")
 
+if __name__ == "__main__":
+    main()
 
---
+++ ex05/building.py
@@ -0,0 +1,55 @@
+import sys
+import string
+
+
+def count(text):
+    """
+    This is the core of the program, here I'm doing all the counts
+
+    Parameters:
+    text (str): the string on which the sums will be performed
+    """
+    numb_of_chars = len(text)
+    numb_of_upper = sum(1 for char in text if char.isupper())
+    numb_of_lower = sum(1 for char in text if char.islower())
+    numb_of_punctuation = sum(1 for char in text if char in string.punctuation)
+    numb_of_spaces = sum(1 for char in text if char.isspace())
+    numb_of_digits = sum(1 for char in text if char.isdigit())
+
+    print(f"The text contains {numb_of_chars} characters")
+    print(f"{numb_of_upper} upper letters")
+    print(f"{numb_of_lower} lower letters")
+    print(f"{numb_of_punctuation} punctuation marks")
+    print(f"{numb_of_spaces} spaces")
+    print(f"{numb_of_digits} digits")
+
+
+def main():
+    """
+    This function counts and displays the sum of a upper-case, lower-case,
+    punctuation characters, digits and spaces (taking the carriage return
+    as a space) in a string passed as parameter.
+
+    Parameters:
+    argv[1]: the text to analyze (if no parameter is provided de program
+    ask for)
+    """
+    try:
+        if len(sys.argv) > 2:
+            raise AssertionError("more than one argument is provided")
+        elif len(sys.argv) == 1:
+            try:
+                text = input("What is the text to count?\n")
+                text += '\n'
+            except EOFError:
+                text = ""
+        else:
+            text = sys.argv[1]
+        count(text)
+    except AssertionError as Err:
+        print(f"Assertion error: {Err}")
+    exit()
+
+
+if __name__ == "__main__":
+    main().__doc__
