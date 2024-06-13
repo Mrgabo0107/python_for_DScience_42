c739d10397a2e75a48405475b7fb16d487d7ddcb
Gabriel Moreno <gdmorenor@unal.edu.co>
2024-06-14 00:21:13 +0200

    ex07 finished

--- ex07/sos.py
+++ ex07/sos.py
@@ -2,45 +2,41 @@ import sys
 
 
 def sos():
-    NESTED_MORSE = {" ": "/ ",
-                    "A": ".- ",
-                    "B": "-... ",
-                    "C": "-.-. ",
-                    "D": "-.. ",
-                    "E": ". ",
-                    "F": "..-. ",
-                    "G": "--. ",
-                    "H": ".... ",
-                    "I": ".. ",
-                    "J": ".--- ",
-                    "K": "--.-- ",
-                    "L": ".-.. ",
-                    "M": "-- ",
-                    "N": "-. ",
-                    "O": "--- ",
-                    "P": ".--. ",
-                    "Q": "--.- ",
-                    "R": ".-. ",
-                    "S": "... ",
-                    "T": "- ",
-                    "U": "..- ",
-                    "V": "...- ",
-                    "W": ".-- ",
-                    "X": "-..- ",
-                    "Y": "-.-- ",
-                    "Z": "--.. ",
-                    "1": ".---- ",
-                    "2": "..--- ",
-                    "3": "...-- ",
-                    "4": "....- ",
-                    "5": "..... ",
-                    "6": "-..... ",
-                    "7": "--... ",
-                    "8": "---.. ",
-                    "9": "----. ",
-                    "0": "----- "}
-    if len(sys.argv) != 2:
-        print("bad arguments", NESTED_MORSE)
+    """
+This function takes a string as an argument and converts it into a Morse
+code message. The function accepts only alphanumeric characters and the
+conventional space (no other whitespace characters).
+
+Parameters:
+- sys.argv[1] (str): The string that will be encoded in Morse code.This
+function takes a string as argument and turn it in a morse message, the
+function accepts only alphanumeric characters and the space.
+
+Returns:
+- str: The Morse code representation of the input string.
+"""
+    NESTED_MORSE = {" ": "/ ", "A": ".- ", "B": "-... ", "C": "-.-. ",
+                    "D": "-.. ", "E": ". ", "F": "..-. ", "G": "--. ",
+                    "H": ".... ", "I": ".. ", "J": ".--- ", "K": "-.- ",
+                    "L": ".-.. ", "M": "-- ", "N": "-. ", "O": "--- ",
+                    "P": ".--. ", "Q": "--.- ", "R": ".-. ", "S": "... ",
+                    "T": "- ", "U": "..- ", "V": "...- ", "W": ".-- ",
+                    "X": "-..- ", "Y": "-.-- ", "Z": "--.. ", "1": ".---- ",
+                    "2": "..--- ", "3": "...-- ", "4": "....- ", "5": "..... ",
+                    "6": "-..... ", "7": "--... ", "8": "---.. ",
+                    "9": "----. ", "0": "----- "}
+    try:
+        if len(sys.argv) != 2 or not all(c.isalnum()
+                                         or (c == ' ') for c in sys.argv[1]):
+            raise AssertionError("AssertionError: the arguments are bad")
+        else:
+            string_to_encode = sys.argv[1].upper()
+            encoded_string = ""
+            for c in string_to_encode:
+                encoded_string += NESTED_MORSE[c]
+            print(encoded_string)
+    except AssertionError as Err:
+        print(f"{Err}")
 
 
 if __name__ == "__main__":
