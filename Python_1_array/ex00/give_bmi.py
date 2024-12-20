f5bc80c795ebb3fff91ff091af1b4ab3c8f4c564
Gabriel Moreno <gdmorenor@unal.edu.co>
2024-12-20 17:55:31 +0100

    starting day two

---
+++ Python_1_array/ex00/give_bmi.py
@@ -0,0 +1,21 @@
+def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+    if not isinstance(height, list) or not isinstance(weight, list):
+        raise TypeError("The height and weight parameters must be a list")
+    if len(height) != len(weight):
+        raise ValueError("The list must have the same length")
+    if not all(isinstance(h, (int, float)) for h in height) or not all(isinstance(w, (int, float)) for w in weight):
+        raise TypeError("All the elements in the lists must be int or float")
+    if not all(h > 0 for h in height) or not all(w > 0 for w in weight):
+        raise ValueError("All height and weight values must be positive")
+    return [w / (h ** 2) for h, w in zip(height, weight)]
+
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    if not isinstance(bmi, list) or not all(isinstance(b, (int, float)) for b in bmi):
+        raise TypeError("The bmi parameter must be a list of int or float")
+    if not isinstance(limit, int):
+        raise ValueError("The limit must be an int")
+    if not all(b > 0 for b in bmi):
+        raise ValueError("All BMI values must be positive")
+    if limit <= 0:
+        raise ValueError("The limit must be a positive integer")
+    return [b > limit for b in bmi]
